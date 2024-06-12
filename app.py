2d5f570b821fa5c278f31861cdda1315196dcfe2
joaothallys <joaothallys>
2024-06-11 23:41:43 -0300

    mudanca de type

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def create_connection():
 # Função para conectar ao S3
 def create_s3_client():
     try:
-        s3_client = boto3.client('s3', 
+        s3_client = boto3.client('s3',
                                  region_name=S3_REGION,
                                  aws_access_key_id=S3_ACCESS_KEY,
                                  aws_secret_access_key=S3_SECRET_KEY)
@@ -64,19 +64,16 @@ def upload_file():
         if file.filename == '':
             return jsonify(error='No selected file'), 400
 
-        # Verificar se o arquivo é do tipo text/csv
-        if file.mimetype != 'text/csv':
-            # Se não for, converter para text/csv
-            file.stream.seek(0)
-            file = io.BytesIO(file.stream.read().decode('utf-8').encode('utf-8-sig'))
-
         filename = secure_filename(file.filename)
         s3_client = create_s3_client()
         if not s3_client:
             return jsonify(error='Failed to connect to S3'), 500
 
         try:
-            s3_client.upload_fileobj(file, S3_BUCKET, filename)
+            # Especificar o tipo de conteúdo do arquivo
+            content_type = file.mimetype
+            s3_client.upload_fileobj(file, S3_BUCKET, filename, ExtraArgs={
+                                     'ContentType': content_type})
         except Exception as e:
             print(f"Failed to upload file to S3: {e}")
             return jsonify(error='Failed to upload file'), 500
@@ -88,8 +85,8 @@ def upload_file():
         if connection:
             try:
                 cursor = connection.cursor()
-                cursor.execute("INSERT INTO files (filename, file_url) VALUES (%s, %s)",
-                               (filename, file_url))
+                cursor.execute("INSERT INTO files (filename, file_url, content_type) VALUES (%s, %s, %s)",
+                               (filename, file_url, content_type))
                 connection.commit()
             except Error as e:
                 print(f"Failed to insert record into MySQL table: {e}")
@@ -103,6 +100,7 @@ def upload_file():
         print(f"Unexpected error during file upload: {e}")
         return jsonify(error='Internal Server Error'), 500
 
+
 # Rota para obter arquivo carregado com tipo condicional
 @app.route('/uploads/<filename>', methods=['GET'])
 def uploaded_file(filename):
@@ -112,19 +110,16 @@ def uploaded_file(filename):
         if connection:
             try:
                 cursor = connection.cursor()
-                cursor.execute("SELECT file_url FROM files WHERE filename = %s", (filename,))
+                cursor.execute(
+                    "SELECT file_url, content_type FROM files WHERE filename = %s", (filename,))
                 row = cursor.fetchone()
                 if row:
                     file_url = row[0]
-                    # Verifica se o tipo do arquivo é CSV
-                    if filename.endswith('.csv'):
-                        # Retorna a URL do arquivo com o tipo /csv
-                        return jsonify(fileUrl=file_url, type='text/csv'), 200
-                    else:
-                        # Retorna a URL do arquivo sem especificar o tipo
-                        return jsonify(fileUrl=file_url), 200
+                    content_type = row[1]
+                    return jsonify(fileUrl=file_url, type=content_type), 200
                 else:
-                    print(f"File not found in database for filename: {filename}")
+                    print(
+                        f"File not found in database for filename: {filename}")
                     return jsonify(error='File not found'), 404
             except Error as e:
                 print(f"Failed to fetch file from MySQL table: {e}")
@@ -135,6 +130,7 @@ def uploaded_file(filename):
     except Exception as e:
         print(f"Unexpected error during file fetch: {e}")
         return jsonify(error='Internal Server Error'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
